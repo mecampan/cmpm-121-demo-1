89a6b64a4ac4b27adf644ed78a338aee742cdafb
mecampan <mecampan@ucsc.edu>
2024-10-08 12:03:03 -0700

    Step 1 Complete: Game Name Change

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "Please work, please change the name without issues thank you very much";
 document.title = gameName;
 
 const header = document.createElement("h1");
