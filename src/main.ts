e27c9ace33d3eff410e4d69464bb264b82b00d2d
mecampan <mecampan@ucsc.edu>
2024-10-08 13:27:22 -0700

    Step 1 Complete: Adding a button

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,22 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Please work, please change the name without issues thank you very much";
+const gameName = "Salt Score";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.textContent = "🧂";
+button.style.padding = "5px";
+button.style.fontSize = "40px";
+
+// Position the button
+button.style.position = "absolute";
+button.style.top = "70%";
+button.style.left = "50%";
+button.style.transform = "translate(-50%, -50%)"; // Center the button using transform
+
+document.body.appendChild(button);
