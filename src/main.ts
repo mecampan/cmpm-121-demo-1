248f0864068883c8e502fbc3bcf4706b6658f1f9
mecampan <mecampan@ucsc.edu>
2024-11-03 14:10:12 -0800

    changed some variable names for easier readability. Swapped around function positions

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
-
 const gameName = "Salt Miner";
 document.title = gameName;
 
@@ -14,22 +13,14 @@ header.style.left = "30%";
 header.style.transform = "translate(-50%, -50%)";
 app.append(header);
 
-// Text display setup
-const div = document.createElement("div");
-div.style.padding = "5px";
-div.style.fontSize = "25px";
-div.style.position = "absolute";
-div.style.top = "32%";
-div.style.left = "30%";
-div.style.transform = "translate(-50%, -50%)";
-document.body.appendChild(div);
-
-let saltNum: number = 0;
-let saltGrowthRate: number = 0;
-const incrementalValue = 1.15;
-const initialX = 70;
-const initialY = 30;
-const buttonSpacing = 10;
+const saltNumText = document.createElement("div");
+saltNumText.style.padding = "5px";
+saltNumText.style.fontSize = "25px";
+saltNumText.style.position = "absolute";
+saltNumText.style.top = "32%";
+saltNumText.style.left = "30%";
+saltNumText.style.transform = "translate(-50%, -50%)";
+document.body.appendChild(saltNumText);
 
 interface Item {
   name: string;
@@ -40,42 +31,79 @@ interface Item {
   button?: HTMLButtonElement;
 }
 
-// Main Salt Clicker Button
-const SaltClickerButton: Item = {
-  name: "🧂",
-  cost: 0,
-  growthRate: 0,
-  purchaseTotal: 0,
-  description: "Click the salt button to get salt. Easy!"
-};
-createButton(SaltClickerButton, 1);
-if (SaltClickerButton.button) {
-  SaltClickerButton.button.style.fontSize = "200px";
-  SaltClickerButton.button.style.padding = "1px";
-  SaltClickerButton.button.style.width = "300px";
-  SaltClickerButton.button.style.left = "30%";
-  SaltClickerButton.button.style.top = "55%";
-}
+let saltNum: number = 0;
+let saltGrowthRate: number = 0;
+const GrowthRateMultiplier = 1.15;
 
 // Purchasable Upgrade Buttons
 const availableItems: Item[] = [
-  { name: "🧂Salt Shakers", cost: 10, growthRate: 1, purchaseTotal: 0, description: "A salt shaker from a restaurant." },
-  { name: "⛏️Pick Axes", cost: 100, growthRate: 3, purchaseTotal: 0, description: "Study tools to help mine for salt." },
-  { name: "🛁Bath Salts", cost: 1000, growthRate: 50, purchaseTotal: 0, description: "Don't snort these, please don't." },
-  { name: "🪨Salt Mines", cost: 10000, growthRate: 500, purchaseTotal: 0, description: "Every thing is edible in here!" },
-  { name: "🧌Internet Trolls", cost: 100000, growthRate: 3000, purchaseTotal: 0, description: "Clacking away on their keyboards at the speed of sound and collecting the delicious tears of all who dare respond." },
-  { name: "👨‍🍳Salt Bae", cost: 1000000, growthRate: 12000, purchaseTotal: 0, description: "$100000 for a gold encrusted steak is a good deal!" },
+  {
+    name: "🧂Salt Shakers",
+    cost: 10,
+    growthRate: 1,
+    purchaseTotal: 0,
+    description: "A salt shaker from a restaurant.",
+  },
+  {
+    name: "⛏️Pick Axes",
+    cost: 100,
+    growthRate: 3,
+    purchaseTotal: 0,
+    description: "Study tools to help mine for salt.",
+  },
+  {
+    name: "🛁Bath Salts",
+    cost: 1000,
+    growthRate: 50,
+    purchaseTotal: 0,
+    description: "Don't snort these, please don't.",
+  },
+  {
+    name: "🪨Salt Mines",
+    cost: 10000,
+    growthRate: 500,
+    purchaseTotal: 0,
+    description: "Every thing is edible in here!",
+  },
+  {
+    name: "🧌Internet Trolls",
+    cost: 100000,
+    growthRate: 3000,
+    purchaseTotal: 0,
+    description:
+      "Clacking away on their keyboards at the speed of sound and collecting the delicious tears of all who dare respond.",
+  },
+  {
+    name: "👨‍🍳Salt Bae",
+    cost: 1000000,
+    growthRate: 12000,
+    purchaseTotal: 0,
+    description: "$100000 for a gold encrusted steak is a good deal!",
+  },
 ];
+
+const initialButtonPosX = 70;
+const initialButtonPosY = 30;
+const buttonSpacing = 10;
+
+function styleButton(button: HTMLButtonElement, index: number) {
+  button.style.padding = "3px";
+  button.style.width = "500px";
+  button.style.fontSize = "30px";
+  button.style.position = "absolute";
+  button.style.left = `${initialButtonPosX}%`;
+  button.style.top = `${initialButtonPosY + index * buttonSpacing}%`;
+  button.style.transform = "translate(-50%, -50%)";
+}
 
 // Set up buttons on initial load
 availableItems.forEach((item, index) => {
   createButton(item, index);
 });
 
-// Functions
 function createButton(item: Item, index: number) {
   const button = document.createElement("button");
-  button.title = item.description; // Set the description as the tooltip
+  button.title = item.description;
   styleButton(button, index);
 
   button.addEventListener("click", () => {
@@ -91,16 +119,6 @@ function createButton(item: Item, index: number) {
   document.body.appendChild(button);
 }
 
-function styleButton(button: HTMLButtonElement, index: number) {
-  button.style.padding = "3px";
-  button.style.width = "500px";
-  button.style.fontSize = "30px";
-  button.style.position = "absolute";
-  button.style.left = `${initialX}%`;
-  button.style.top = `${initialY + index * buttonSpacing}%`;
-  button.style.transform = "translate(-50%, -50%)";
-}
-
 function manualIncrement() {
   saltNum++;
   updateDisplay();
@@ -108,17 +126,11 @@ function manualIncrement() {
 
 function incrementGrowthRate(item: Item) {
   saltNum -= item.cost;
-  item.cost *= incrementalValue;
+  item.cost *= GrowthRateMultiplier;
   saltGrowthRate += item.growthRate;
   item.purchaseTotal++;
   updateButtonDisplay(item);
   updateDisplay();
-}
-
-function checkButtonStatus(item: Item) {
-  if (item.button) {
-    item.button.disabled = saltNum < item.cost;
-  }
 }
 
 function updateButtonDisplay(item: Item) {
@@ -130,10 +142,34 @@ function updateButtonDisplay(item: Item) {
 }
 
 function updateDisplay() {
-  div.innerHTML = `
+  saltNumText.innerHTML = `
     ${saltNum.toFixed(0)}: Pounds of Salt<br>
     per second: ${saltGrowthRate.toFixed(1)}
   `;
+}
+
+function checkButtonStatus(item: Item) {
+  if (item.button) {
+    item.button.disabled = saltNum < item.cost;
+  }
+}
+
+// Main Salt Clicker Button
+const SaltClickerButton: Item = {
+  name: "🧂",
+  cost: 0,
+  growthRate: 0,
+  purchaseTotal: 0,
+  description: "Click the salt button to get salt. Easy!",
+};
+
+createButton(SaltClickerButton, 1);
+if (SaltClickerButton.button) {
+  SaltClickerButton.button.style.fontSize = "200px";
+  SaltClickerButton.button.style.padding = "1px";
+  SaltClickerButton.button.style.width = "300px";
+  SaltClickerButton.button.style.left = "30%";
+  SaltClickerButton.button.style.top = "55%";
 }
 
 // Main animation loop
